50873f153c116c6b743d936fcbc3c0402e27c70f
Sean Wang <seanwaterloo2997@gmail.com>
2023-10-07 14:17:56 -0400

    Updates

--- prices_petite/members/views.py
+++ prices_petite/members/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.template import loader
 
 def members(request):
-    template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+  template = loader.get_template('first.html')
+  return HttpResponse(template.render())
