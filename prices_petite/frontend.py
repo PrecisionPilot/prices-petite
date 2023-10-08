b8a7f4dbffbd511fa55f4de51417187378abe2ea
David-Ykz <kzdavid314@gmail.com>
2023-10-08 10:30:28 -0400

    added toggle feature (wip)

--- prices_petite/frontend.py
+++ prices_petite/frontend.py
@@ -1,14 +1,17 @@
+import webbrowser
+
 from taipy.gui import Gui
 import pandas as pd
 from webscrapper import search_web_prices
-from python import capture
+#from python import capture
 
 user_query = ""
 display_search_results = False
 data = {"Vendor": [], "Price": [], "Description": [], "Url": []}
 dataframe = pd.DataFrame.from_dict(data)
-
-
+original_dataframe = dataframe
+vendor_toggle_options = ["Show All", "Amazon", "Etsy", "Walmart", "Ebay", "Other"]
+selected_vendor = vendor_toggle_options[0]
 page = """
 
 # Prices Petite
@@ -20,9 +23,9 @@ Enter any product:
 
 <|Open Camera|button|on_action=on_capture_button|>
 
-
 <|part|render={display_search_results}|
 <|{dataframe}|table|>
+<|{selected_vendor}|on_change=update_dataframe|toggle|lov={vendor_toggle_options}|>
 |>
 
 """
@@ -32,11 +35,25 @@ def on_button_action(state):
     if state.user_query != "":
         new_dataframe = search_web_prices(state.user_query)
         state.dataframe = new_dataframe
+        state.original_dataframe = new_dataframe
         state.display_search_results = True
 
 def on_capture_button(state):
-    state.user_query = capture()
+#    state.user_query = capture()
     on_button_action(state)
+
+def update_dataframe(state):
+    if state.selected_vendor == state.vendor_toggle_options[0]:
+        state.dataframe = state.original_dataframe
+    elif state.selected_vendor == state.vendor_toggle_options[len(state.vendor_toggle_options) - 1]:
+        for vendor in state.vendor_toggle_options:
+            state.dataframe = state.dataframe.drop(state.dataframe[vendor in state.dataframe["Vendor"]].index)
+    else:
+        print("In else")
+        print(state.dataframe[state.selected_vendor not in state.dataframe["Vendor"]])
+        state.dataframe = state.dataframe.drop(state.dataframe[state.selected_vendor not in state.dataframe["Vendor"]].index)
+
+
 
 def on_change(state, var_name, var_value):
     if var_name == "text" and var_value == "Reset":
