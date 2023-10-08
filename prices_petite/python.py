144b079c694d3fee55fe1b01e08c9902b3437bad
Sean Wang <seanwaterloo2997@gmail.com>
2023-10-08 10:22:53 -0400

    Add "Press Enter to Capture"

--- prices_petite/python.py
+++ prices_petite/python.py
@@ -1,8 +1,9 @@
 from cloud import detect_objects, detect_text
 import cv2
 
+
 def capture():
-    cv2.namedWindow("preview")
+    cv2.namedWindow("Capture")
     vc = cv2.VideoCapture(0)
 
     keyword = ""
@@ -13,21 +14,24 @@ def capture():
         rval = False
 
     while rval:
-        cv2.imshow("preview", frame)
+        cv2.imshow("Capture", frame)
         rval, frame = vc.read()
+        frame_notext = frame.copy()
+        cv2.putText(frame, "Press Enter to Capture", (120, 30), cv2.FONT_HERSHEY_PLAIN, 2, (200, 128, 0), 2)
         key = cv2.waitKey(20)
+
         if key == 13: # return key   --- Capture ---
-            cv2.imwrite('capture.jpg', frame)
+            cv2.imwrite('capture.jpg', frame_notext)
             if "lay's" in detect_text("capture.jpg"):
                 keyword = "Lay's chips"
             else:
                 keyword = detect_objects("capture.jpg")
-            break
+            break            
         elif key == 27: # exit on ESC
             break
 
     vc.release()
-    cv2.destroyWindow("preview")
+    cv2.destroyWindow("Capture")
 
     return keyword
 
