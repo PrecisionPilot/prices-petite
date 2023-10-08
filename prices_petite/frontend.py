2e015a4133c025301c25a6cc405c9d2c5a8de4bc
Sean Wang <seanwaterloo2997@gmail.com>
2023-10-08 00:28:47 -0400

    THIS SHI WORKS SUIIIIII

--- prices_petite/frontend.py
+++ prices_petite/frontend.py
@@ -1,6 +1,7 @@
 from taipy.gui import Gui
 import pandas as pd
-import webscrapper
+from webscrapper import search_web_prices
+from python import capture
 
 user_query = ""
 display_search_results = False
@@ -17,6 +18,9 @@ Enter any product:
 <|{user_query}|input|>
 <|Search|button|on_action=on_button_action|>
 
+<|Open Camera|button|on_action=on_capture_button|>
+
+
 <|part|render={display_search_results}|
 <|{dataframe}|table|>
 |>
@@ -26,9 +30,13 @@ Enter any product:
 
 def on_button_action(state):
     if state.user_query != "":
-        new_dataframe = webscrapper .search_web_prices(state.user_query)
+        new_dataframe = search_web_prices(state.user_query)
         state.dataframe = new_dataframe
         state.display_search_results = True
+
+def on_capture_button(state):
+    state.user_query = capture()
+    on_button_action(state)
 
 def on_change(state, var_name, var_value):
     if var_name == "text" and var_value == "Reset":
@@ -39,5 +47,6 @@ stylekit = {
     "color_primary": "#9CEAEF",
     "color_secondary": "#746D75",
 }
+
 Gui(page).run(stylekit=stylekit)
 
---
+++ prices_petite/python.py
@@ -0,0 +1,32 @@
+from cloud import detect_objects, detect_text
+import cv2
+
+def capture():
+    cv2.namedWindow("preview")
+    vc = cv2.VideoCapture(0)
+
+    keyword = ""
+
+    if vc.isOpened(): # try to get the first frame
+        rval, frame = vc.read()
+    else:
+        rval = False
+
+    while rval:
+        cv2.imshow("preview", frame)
+        rval, frame = vc.read()
+        key = cv2.waitKey(20)
+        if key == 13: # return key   --- Capture ---
+            cv2.imwrite('capture.jpg', frame)
+            if "lay's" in detect_text("capture.jpg"):
+                keyword = "Lay's chips"
+            else:
+                keyword = detect_objects("capture.jpg")
+            break
+        elif key == 27: # exit on ESC
+            break
+
+    vc.release()
+    cv2.destroyWindow("preview")
+
+    return keyword
