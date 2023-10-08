a1d4715a927e0330243a43a7467e2405e142c400
David-Ykz <kzdavid314@gmail.com>
2023-10-07 20:16:04 -0400

    added hideable table

--- prices_petite/frontend.py
+++ prices_petite/frontend.py
@@ -1,41 +1,43 @@
 from taipy.gui import Gui
 import pandas as pd
+#import scraping_test
 
-text = "Original text"
-buttonPressed = False
-dataframe = ""
-
-searchPage = """
-<|{text}|input|>
-
-<|Search|button|on_action=on_button_action|>
-
-<|{dataframe}|table|>
-
-"""
-
-resultPage = """
-# Getting started with Taipy GUI
-
-<|{dataframe}|table|>
-"""
-
-
-def on_button_action(state):
-    print(state.text)
-    state.
-
-def on_change(state, var_name, var_value):
-    if var_name == "text" and var_value == "Reset":
-        state.text = ""
-        return
-
-
-dataframe = pd.DataFrame({"Text":[text, 'Other', 'Love'],
+user_query = "Original text"
+display_search_results = False
+dataframe = pd.DataFrame({"Text":[user_query, 'Other', 'Love'],
                           "Score Pos":[1, 1, 4],
                           "Score Neu":[2, 3, 1],
                           "Score Neg":[1, 2, 0],
                           "Overall":[0, -1, 4]})
 
-Gui(searchPage).run(use_reload=True)
+page = """
+<|layout|columns=1 1|
+<|
+<|{user_query}|input|>
+    
+<|Search|button|on_action=on_button_action|>
+
+<|part|render={display_search_results}|
+<|{dataframe}|table|>
+|>
+
+|>
+
+"""
+
+
+def on_button_action(state):
+    print(state.user_query)
+    state.display_search_results = not state.display_search_results
+
+
+
+def on_change(state, var_name, var_value):
+    if var_name == "text" and var_value == "Reset":
+        state.user_query = ""
+        return
+
+
+
+Gui(page).run()
 
